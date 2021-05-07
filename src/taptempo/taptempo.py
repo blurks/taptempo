81aff0aff06dc089bcf006200417e7ebc2ad1a76
blurks <blrks@nevermore3.fake>
2021-05-07 02:16:12 +0200

    added midi support

--- src/taptempo/taptempo.py
+++ src/taptempo/taptempo.py
@@ -1,7 +1,7 @@
 import time
 import tkinter as tk
 from tkinter import ttk
-
+import rtmidi
 
 class TapTempo:
     """Model to tap in a tempo."""
@@ -32,6 +32,94 @@ class TapTempo:
         self.bpmAvg = 0.0
 
 
+class MidiInterface:
+    """Open a midi port and listen for events. For every Note-On event, call a callback."""
+    def __init__(self, callback):
+        self.tapCallback = callback
+        self.reset()
+        
+    def reset(self):
+        self.midiin = rtmidi.RtMidiIn()
+        self.midiin.setCallback(self.midiCallback)
+        self.portNumber = 0
+        self.portName = ""
+        self.isConnected = False
+
+    def getPorts(self):
+        portN = self.midiin.getPortCount()
+        return dict([(i, self.midiin.getPortName(i)) for i in range(portN)])
+
+    def openPort(self, port: int):
+        try:
+            self.midiin.openPort(port)
+            self.portNumber = port
+            self.portName = self.midiin.getPortName(port)
+            self.isConnected = True
+        except:
+            self.portNumber = 0
+            self.portName = ""
+            self.isConnected = False
+        return self.isConnected
+
+    def closePort(self):
+        self.midiin.closePort()
+        self.reset()
+
+    def midiCallback(self, midiE):
+        if midiE.isNoteOn():
+            self.tapCallback()
+    
+
+class MidiStatusBar(ttk.Frame):
+    """Status bar that shows info about the MIDI interface and provides
+    some configuration options."""
+    def __init__(self, master, midiInterface):
+        ttk.Frame.__init__(self, master, padding=(4,0,4,0), borderwidth=2, relief="sunken")
+        self.midi = midiInterface
+
+        ttk.Label(self, text="MIDI:").grid(row=0, column=0)
+        self.midiStatusLabel = ttk.Label(self, text=self.midi.portName)
+        self.midiStatusLabel.grid(row=0, column=1, sticky="nwse")
+
+        self.optionsButton = ttk.Button(self, text="⚙", width=-1,
+                                        command=self.showOptions)
+        self.optionsButton.grid(row=0, column=2, sticky="e")
+        self.grid_columnconfigure(1, weight=1)
+
+    def update(self):
+        self.midiStatusLabel["text"] = self.midi.portName
+
+    def showOptions(self):
+        optionsDialog = tk.Toplevel(self)
+        optionsDialog.title("Chose a midi port")
+        optionsFrame = ttk.Frame(optionsDialog, padding=(4,4,4,4))
+        optionsFrame.grid(row=0, column=0, sticky="nwse")
+        ttk.Label(optionsFrame, text="Chose a midi port").grid(column=0, row=0, columnspan=2)
+
+        rbtnFrame = ttk.Frame(optionsFrame)
+        portVar = tk.IntVar()
+        portVar.set(self.midi.portNumber)
+
+        ports = self.midi.getPorts()
+        for pNum in ports.keys():
+            rbtn = tk.Radiobutton(rbtnFrame, text=ports[pNum],
+                                  variable=portVar, value=pNum)
+            rbtn.grid(sticky="W")
+        rbtnFrame.grid(row=1, column=0, columnspan=2, sticky="W")
+
+        def ok():
+            if self.midi.portNumber != portVar.get():
+                self.midi.closePort()
+                self.midi.openPort(portVar.get())
+            self.update()
+            optionsDialog.destroy()
+            
+        okButton = ttk.Button(optionsFrame, text="Ok", command=ok)
+        okButton.grid(row=2, column=0)
+
+        cancelButton = ttk.Button(optionsFrame, text="Cancel", command=optionsDialog.destroy)
+        cancelButton.grid(row=2, column=1)
+
 class TapTempoGui:
     """Tk-App to tap-in a tempo."""
     def __init__(self, root, tempo=TapTempo()):
@@ -42,12 +130,17 @@ class TapTempoGui:
         self.strBPMAVG = tk.StringVar()
         self.update()
 
-        # Root and Mainframe
         self.root = root
 
+        # main app
         self.mainframe = ttk.Frame(self.root, padding="4 4 4 4")
         self.mainframe.grid(column=0, row=0)
 
+        self.midi = MidiInterface(self.tap)
+        print(self.midi.openPort(0))
+        self.statusBar = MidiStatusBar(self.root, self.midi)
+        self.statusBar.grid(column=0, row=1, sticky="nwse")
+        
         # BPM Labels
         ttk.Label(self.mainframe, text="BPM:", justify="right").grid(column=0, row=0)
         self.labelBPM = ttk.Label(self.mainframe, textvariable=self.strBPM)
